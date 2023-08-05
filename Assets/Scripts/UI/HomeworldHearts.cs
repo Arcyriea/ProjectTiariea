b5c7f41972d1479124728e8854336f3ba7837e02
SubsectorKirisakyia <108776309+SubsectorKirisakyia@users.noreply.github.com>
2023-08-05 07:41:45 +0700

    Improve Heart Count SCRIPT for Homeworld

--- Assets/Scripts/UI/HomeworldHearts.cs
+++ Assets/Scripts/UI/HomeworldHearts.cs
@@ -18,7 +18,7 @@ public class HomeworldHearts : MonoBehaviour
     public Sprite emptyHeart;
 
     private enum HeartCount { I, II, III, IV, V, VI, VII, VIII, IX, X }
-    private static HeartCount[] enumValues = (HeartCount[]) Enum.GetValues(typeof(HeartCount));
+    private static HeartCount[] enumValues = (HeartCount[])Enum.GetValues(typeof(HeartCount));
 
     private static HeartCount heartCount = enumValues[0];
     // Start is called before the first frame update
@@ -28,15 +28,40 @@ public class HomeworldHearts : MonoBehaviour
     {
         if (heart > numOfHearts) heart = numOfHearts;
 
+        UpdateHearts();
+        UpdateTextHeart();
+    }
+
+    private void UpdateHearts()
+    {
         for (int i = 0; i < hearts.Length; i++)
         {
-            if (i < heart)
+            if (i == 0 && heart > 10)
             {
                 hearts[i].sprite = fullHeart;
-            } else
+            }
+            else if (i < heart % 10)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
             {
                 hearts[i].sprite = emptyHeart;
             }
+        }
+    }
+
+    private void UpdateTextHeart()
+    {
+        int tier = heart > 100 ? 9 : (heart - 1) / 10; // Calculate the tier based on heart count
+
+        
+        if (tier < enumValues.Length)
+        {
+            heartCount = enumValues[tier];
+
+            if (heart > 100) textHeart.text = heartCount.ToString() + ": " +  heart.ToString();
+            else textHeart.text = heartCount.ToString();
         }
     }
 }
