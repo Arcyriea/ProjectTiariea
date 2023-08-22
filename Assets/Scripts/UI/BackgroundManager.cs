7768e35f0a63b327bbeb75a8da139f6cf51ac18c
SubsectorKirisakyia <108776309+SubsectorKirisakyia@users.noreply.github.com>
2023-08-22 21:42:03 +0700

    Adding Background Controller

---
+++ Assets/Scripts/UI/BackgroundManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float scrollSpeed = 0f;
+    private float offset;
+    private Material mat;
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scrollSpeed = PartyController.partyTravelSpeed / 100f;
+        offset += (Time.deltaTime * scrollSpeed) / 10f;
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
